e9d53166907a7cb2a383f3df183d30bfb60a69a9
Admin <Admin@DESKTOP-H04GFR6>
2024-03-23 21:32:13 +0530

    Commit1

---
+++ R1/src/main/java/RepoTest1/R1/App.java
@@ -0,0 +1,13 @@
+package RepoTest1.R1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ R1/src/main/java/RepoTest1/R1/Test1.java
@@ -0,0 +1,10 @@
+package RepoTest1.R1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
